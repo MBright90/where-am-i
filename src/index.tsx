415fe1ae50857f1621f473b97287e3d71ea1db25
mbright90 <michael.bright90@icloud.com>
2023-04-10 19:06:33 +0100

    Update imports to use alias

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
+import App from '@components/app'
+import '@style/style.scss'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './components/app'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-)
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
-
