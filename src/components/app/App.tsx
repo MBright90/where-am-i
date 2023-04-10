15e16950d44d50a4fa0fe0d26068baa91895949a
mbright90 <michael.bright90@icloud.com>
2023-04-10 19:02:57 +0100

    Restructure and display imported components

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,13 +1,18 @@
+import Footer from '@components/footer'
+import MainImage from '@components/mainImage'
 import React from 'react'
 
+import robotCity from '../../assets/images/robot-city.jpg'
 import style from './App.module.scss'
 
 const App: React.FC = () => {
   return (
     <div className={style.app}>
-      I am working
+      <p>I am working</p>
+      <MainImage imageURL={robotCity} />
+      <Footer />
     </div>
   )
 }
 
-export default App;
+export default App
