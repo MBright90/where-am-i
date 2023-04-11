64b422d96844a56061f8b4493d163cba31da334e
mbright90 <michael.bright90@icloud.com>
2023-04-11 17:06:51 +0100

    Adapt footer positioning and structure

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,17 +7,13 @@ import style from './Footer.module.scss'
 const Footer: React.FC = () => {
   return (
     <React.Fragment>
-      <div className={style.createdBy}>
-        <a href="https://github.com/MBright90/where-am-i">
-          <FontAwesomeIcon icon={faGithub} /> MBright90
-        </a>
-      </div>
+      <a className={style.createdBy} href="https://github.com/MBright90/where-am-i">
+        <FontAwesomeIcon icon={faGithub} /> MBright90
+      </a>
 
-      <div className={style.credits}>
-        <a href="https://www.instagram.com/ad.2.222/">
-          <FontAwesomeIcon icon={faInstagram} /> Egor Klyuchnyk
-        </a>
-      </div>
+      <a className={style.credits} href="https://www.instagram.com/ad.2.222/">
+        <FontAwesomeIcon icon={faInstagram} /> Egor Klyuchnyk
+      </a>
     </React.Fragment>
   )
 }
