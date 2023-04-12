b188511e5839cf10f9a03055c4daab1fd5691a0c
mbright90 <michael.bright90@icloud.com>
2023-04-12 10:43:44 +0100

    Import timer component for styling

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,17 @@
 import robotCity from '@assets/images/robot-city.jpg'
 import Footer from '@components/footer'
 import MainImage from '@components/mainImage'
+import Timer from '@components/timer'
 import React from 'react'
 
 import style from './App.module.scss'
 
+const gameIsStarted = true
+
 const App: React.FC = () => {
   return (
     <div className={style.appOverlay}>
+      {gameIsStarted ? <Timer /> : null}
       <MainImage imageURL={robotCity} />
       <Footer />
     </div>
