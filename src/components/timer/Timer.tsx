5fc5fc2b62efbd61853232fa219a31ae2bec27c8
mbright90 <michael.bright90@icloud.com>
2023-04-13 10:34:27 +0100

    Implement timer counting functionality

--- src/components/timer/Timer.tsx
+++ src/components/timer/Timer.tsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import { faClock } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useEffect, useState } from 'react'
 
 import style from './Timer.module.scss'
 
 const Timer: React.FC = () => {
-  const timerValue = 100
+  const [timeCount, setTimeCount] = useState(0)
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeCount((count) => count + 1)
+    }, 1000)
+    return () => clearInterval(timer)
+  }, [])
+
   return (
     <div className={style.timerContainer}>
-      <p>{timerValue}</p>
+      <FontAwesomeIcon icon={faClock} />
+      <p>{timeCount}</p>
     </div>
   )
 }
