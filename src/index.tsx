9673b7eaa94145484cb7f7a97139f23ef838ff22
mbright90 <michael.bright90@icloud.com>
2023-04-15 13:26:05 +0100

    Init firebase app

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
 import '@style/style.scss'
+import { initializeApp } from 'firebase/app'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './app'
+import firebaseConfig from './app/firebase/firebaseConfig'
+
+try {
+  initializeApp(firebaseConfig)
+} catch (error) {
+  console.log('Could not initialize Firestore', error)
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
