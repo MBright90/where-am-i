f905241b7b1255e7f747344c63bd43fee54110fd
mbright90 <michael.bright90@icloud.com>
2023-04-15 10:12:10 +0100

    Restructure files of hud elements and import

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,6 @@
 import robotCity from '@assets/images/robot-city.jpg'
-import Footer from '@components/footer'
+import { Footer, Timer } from '@components/hud'
 import MainImage from '@components/mainImage'
-import Timer from '@components/timer'
 import React from 'react'
 
 import style from './App.module.scss'
