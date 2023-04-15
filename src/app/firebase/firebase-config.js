3f1305bcf1f1447b0a691cefdb7f167bc70d83b3
mbright90 <michael.bright90@icloud.com>
2023-04-15 11:51:41 +0100

    Init firebase config file

---
+++ src/app/firebase/firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'secret',
+  authDomain: 'secret',
+  projectId: 'secret',
+  storageBucket: 'secret',
+  messagingSenderId: 'secret',
+  appId: 'secret'
+}
+
+export default firebaseConfig
