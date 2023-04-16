69d29248d782300a726e4821719dc3aeb8053a66
mbright90 <michael.bright90@icloud.com>
2023-04-16 13:36:57 +0100

    Add custome types file to support DRY

---
+++ src/customTypes/types.ts
@@ -0,0 +1,11 @@
+export interface Character {
+  name: string
+  characterId: string
+  storeId?: string
+  image?: string
+  hasFound?: boolean
+  highX?: number
+  highY?: number
+  lowX?: number
+  lowY?: number
+}
