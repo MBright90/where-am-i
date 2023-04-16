f5d9266b2b6364f9ab0eeb7f5b656162101384fd
mbright90 <michael.bright90@icloud.com>
2023-04-16 13:48:03 +0100

    Add function to toggle setlist

--- src/components/hud/checklist/Checklist.tsx
+++ src/components/hud/checklist/Checklist.tsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import { AppContext } from '@app/context/Context'
+import { Character } from '@customTypes/types'
+import React, { ReactNode, useContext, useState } from 'react'
 
 import style from './Checklist.module.scss'
+import CheckItem from './checkItem/CheckItem'
 
-interface props {
+interface ChecklistProps {
   imageId: string
 }
 
-const Checklist: React.FC<props> = ({ imageId }) => {
-  console.log(imageId)
-  return <div className={style.checklist}></div>
+const Checklist: React.FC<ChecklistProps> = () => {
+  const [isShowing, setIsShowing] = useState<boolean>(false)
+  const { currentCharacters } = useContext(AppContext)
+
+  const handleToggleClick = () => {
+    setIsShowing(!isShowing)
+  }
+
+  const checkItemArr: Array<ReactNode> = currentCharacters.map((character: Character) => (
+    <CheckItem character={character} key={character.characterId} />
+  ))
+
+  return (
+    <div
+      className={style.checklistToggle}
+      onClick={handleToggleClick}
+      onKeyDown={(e) => {
+        if (e.key === 'enter') handleToggleClick()
+      }}
+    >
+      {isShowing ? <div className={style.checklist}>{checkItemArr}</div> : null}
+    </div>
+  )
 }
 
 export default Checklist
