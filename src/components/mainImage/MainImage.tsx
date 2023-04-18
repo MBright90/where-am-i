daefe9bc677ba99e9c2230c44977096100944c25
mbright90 <michael.bright90@icloud.com>
2023-04-18 17:58:12 +0100

    Add logic to show SelectionMenu component onClick

--- src/components/mainImage/MainImage.tsx
+++ src/components/mainImage/MainImage.tsx
@@ -1,15 +1,18 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { MouseEventHandler } from 'react'
+import React, { MouseEventHandler, useState } from 'react'
 
 import style from './MainImage.module.scss'
+import SelectionMenu from './selectionMenu/SelectionMenu'
 
 interface MainImageProps {
   imageURL?: string
 }
 
 const MainImage: React.FC<MainImageProps> = ({ imageURL }) => {
+  const [menuShowing, setMenuShowing] = useState({ isShowing: false, menuX: 0, menuY: 0 })
+
   const convertToPercentage = (position: number, totalLength: number) => {
     return (position / totalLength) * 100
   }
@@ -20,16 +23,26 @@ const MainImage: React.FC<MainImageProps> = ({ imageURL }) => {
 
     const xPosPercent = convertToPercentage(e.clientX - rect.left, e.currentTarget.width)
     const yPosPercent = convertToPercentage(e.clientY - rect.top, e.currentTarget.height)
-    return { X: xPosPercent, Y: yPosPercent }
+
+    setMenuShowing({
+      isShowing: true,
+      menuX: xPosPercent,
+      menuY: yPosPercent
+    })
   }
 
   return (
-    <img
-      className={style.mainImage}
-      src={imageURL}
-      alt="mainImage"
-      onClick={retrieveCoordsPercentage}
-    />
+    <React.Fragment>
+      <img
+        className={style.mainImage}
+        src={imageURL}
+        alt="mainImage"
+        onClick={retrieveCoordsPercentage}
+      />
+      {menuShowing.isShowing ? (
+        <SelectionMenu menuX={menuShowing.menuX} menuY={menuShowing.menuY} />
+      ) : null}
+    </React.Fragment>
   )
 }
 
