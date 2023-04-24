e7d0881ab464c47da41abde3daa6f7a410ac3058
mbright90 <michael.bright90@icloud.com>
2023-04-24 09:58:06 +0100

    Add SettingsMenu component

--- src/components/hud/HUD.tsx
+++ src/components/hud/HUD.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 import Checklist from './checklist/Checklist'
 import Footer from './footer/Footer'
+import SettingsMenu from './settingsMenu/SettingsMenu'
 import Timer from './timer/Timer'
 
 const HUD: React.FC = () => {
   return (
     <div className="hud">
       <Timer />
+      <SettingsMenu />
       <Checklist />
       <Footer />
     </div>
