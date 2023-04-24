6367b96421d780ff12caf87645ea4b7185f59b8b
mbright90 <michael.bright90@icloud.com>
2023-04-24 16:27:10 +0100

    Add functionality to show and remove feedback

--- src/components/mainImage/MainImage.tsx
+++ src/components/mainImage/MainImage.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { MouseEventHandler, useState } from 'react'
+import React, { MouseEventHandler, useEffect, useState } from 'react'
 
 import style from './MainImage.module.scss'
+import FeedbackIcon from './feedbackIcon/FeedbackIcon'
 import SelectionMenu from './selectionMenu/SelectionMenu'
 
 interface MainImageProps {
@@ -12,6 +13,12 @@ interface MainImageProps {
 
 const MainImage: React.FC<MainImageProps> = ({ imageURL }) => {
   const [menuShowing, setMenuShowing] = useState({ isShowing: false, posX: 0, posY: 0 })
+  const [iconShowing, setIconShowing] = useState({
+    isShowing: false,
+    posX: 0,
+    posY: 0,
+    outcome: false
+  })
 
   const convertToPercentage = (position: number, totalLength: number) => {
     return (position / totalLength) * 100
@@ -48,6 +55,31 @@ const MainImage: React.FC<MainImageProps> = ({ imageURL }) => {
     })
   }
 
+  const showFeedback = (outcome: boolean, posX: number, posY: number) => {
+    setIconShowing({
+      isShowing: true,
+      posX,
+      posY,
+      outcome
+    })
+  }
+
+  useEffect(() => {
+    if (iconShowing.isShowing) {
+      const timeoutId = setTimeout(
+        () =>
+          setIconShowing({
+            isShowing: false,
+            posX: 0,
+            posY: 0,
+            outcome: false
+          }),
+        950
+      )
+      return () => clearTimeout(timeoutId)
+    }
+  }, [iconShowing.isShowing])
+
   return (
     <React.Fragment>
       <img className={style.mainImage} src={imageURL} alt="mainImage" onClick={handleImageClick} />
@@ -56,6 +88,14 @@ const MainImage: React.FC<MainImageProps> = ({ imageURL }) => {
           posX={menuShowing.posX}
           posY={menuShowing.posY}
           handleMenuClick={handleMenuClick}
+          showFeedback={showFeedback}
+        />
+      ) : null}
+      {iconShowing.isShowing ? (
+        <FeedbackIcon
+          outcome={iconShowing.outcome}
+          posX={iconShowing.posX}
+          posY={iconShowing.posY}
         />
       ) : null}
     </React.Fragment>
