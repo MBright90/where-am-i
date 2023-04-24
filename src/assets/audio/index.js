dffec6de7aa85875f7ae33a47c69d2ec43a1bda1
mbright90 <michael.bright90@icloud.com>
2023-04-24 15:28:43 +0100

    Resolve issue in audio file import

--- src/assets/audio/index.js
+++ src/assets/audio/index.js
@@ -1,4 +1,5 @@
 import correctAudio from './correctAudio.mp3'
 import incorrectAudio from './incorrectAudio.mp3'
+import victoryAudio from './victoryAudio.mp3'
 
-export { correctAudio, incorrectAudio }
+export { correctAudio, incorrectAudio, victoryAudio }
