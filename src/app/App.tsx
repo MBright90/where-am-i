c3adf07dcd3560d1d80a167ad28799000e59b9b4
mbright90 <michael.bright90@icloud.com>
2023-04-26 10:44:25 +0100

    Add conditional rendering of VictoryScreen

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,12 +2,13 @@ import { AppContext } from '@app/context'
 import HUD from '@components/hud'
 import MainImage from '@components/mainImage'
 import StartScreen from '@components/startScreen/StartScreen'
+import VictoryScreen from '@components/victoryScreen/VictoryScreen'
 import React, { useContext } from 'react'
 
 import style from './App.module.scss'
 
 const App: React.FC = () => {
-  const { currentImage, gameIsStarted } = useContext(AppContext)
+  const { currentImage, gameIsStarted, isVictorious } = useContext(AppContext)
 
   // Add any further image IDs to these props to add them to app
   const gamePlayStart = <StartScreen locationImageIdArr={['robot-city', 'robot-city']} />
@@ -24,6 +25,7 @@ const App: React.FC = () => {
       className={style.appOverlay}
       style={gameIsStarted ? { minWidth: 900 + 'px' } : { minWidth: 100 + '%' }}
     >
+      {isVictorious ? <VictoryScreen /> : null}
       {gameIsStarted ? gamePlayActive : gamePlayStart}
     </div>
   )
