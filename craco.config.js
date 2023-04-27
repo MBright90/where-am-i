3ac15ababca5bef2ca5460675fdc83a6bc235955
mbright90 <michael.bright90@icloud.com>
2023-04-27 10:35:25 +0100

    Add image optimization config

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,5 @@
 const path = require('path')
-
-// const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin')
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin')
 
 module.exports = {
   webpack: {
@@ -32,42 +31,22 @@ module.exports = {
           ]
         }
       ]
+    },
+    optimization: {
+      minimizer: [
+        new ImageMinimizerPlugin({
+          minimizer: {
+            implementation: ImageMinimizerPlugin.imageminMinify,
+            options: {
+              plugins: [
+                ['gifsicle', { interlaced: true }],
+                ['jpegtran', { progressive: true }],
+                ['optipng', { optimizationLevel: 5 }]
+              ]
+            }
+          }
+        })
+      ]
     }
-    // optimization: {
-    //   minimizer: [
-    //     new ImageMinimizerPlugin({
-    //       minimizer: {
-    //         implementation: ImageMinimizerPlugin.imageminMinify,
-    //         options: {
-    //           plugins: [
-    //             ['gifsicle', { interlaced: true }],
-    //             ['jpegtran', { progressive: true }],
-    //             ['optipng', { optimizationLevel: 5 }],
-    //             [
-    //               'svgo',
-    //               {
-    //                 plugins: [
-    //                   {
-    //                     name: 'preset-default',
-    //                     params: {
-    //                       overrides: {
-    //                         removeViewBox: false,
-    //                         addAttributesToSVGElement: {
-    //                           params: {
-    //                             attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }]
-    //                           }
-    //                         }
-    //                       }
-    //                     }
-    //                   }
-    //                 ]
-    //               }
-    //             ]
-    //           ]
-    //         }
-    //       }
-    //     })
-    //   ]
-    // }
   }
 }
