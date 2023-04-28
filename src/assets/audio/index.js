38229cdc1029abfdb605974e5aa9f7d780f13d63
mbright90 <michael.bright90@icloud.com>
2023-04-28 10:18:07 +0100

    Update audio export file

--- src/assets/audio/index.js
+++ src/assets/audio/index.js
@@ -1,5 +1,6 @@
 import correctAudio from './correctAudio.mp3'
 import incorrectAudio from './incorrectAudio.mp3'
+import introAudio from './introAudio.mp3'
 import victoryAudio from './victoryAudio.mp3'
 
-export { correctAudio, incorrectAudio, victoryAudio }
+export { correctAudio, incorrectAudio, introAudio, victoryAudio }
