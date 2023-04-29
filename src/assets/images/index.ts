fe0e27f25bb44f6db9558e7c1bfc49e0ca726f5d
mbright90 <michael.bright90@icloud.com>
2023-04-29 15:35:19 +0100

    Update character and location database

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -6,6 +6,12 @@ import megGriffin from './meg-griffin.png'
 import mikeWazowski from './mike-wazowski.png'
 import robotCity from './robot-city.jpg'
 import tick from './tick.png'
+import universeImg from './universe-113.jpg'
+import bobMinion from './bob-minion.png'
+import gandalf from './gandalf.png'
+import geraltOfRivia from './geralt-of-rivia.png'
+import predator from './the-predator.png'
+import KeanuReeves from './keanu-reeves.png'
 
 interface ImageDatabase {
   [key: string]: string
@@ -16,11 +22,17 @@ const characterImageDatabase: ImageDatabase = {
   'general-grievous': generalGrievous,
   'kang-johnson': kangJohnson,
   'meg-griffin': megGriffin,
-  'mike-wazowski': mikeWazowski
+  'mike-wazowski': mikeWazowski,
+  'bob-minion': bobMinion,
+  'gandalf': gandalf,
+  'geralt-of-rivia': geraltOfRivia,
+  'the-predator': predator,
+  'keanu-reeves': KeanuReeves
 }
 
 const locationImageDatabase: ImageDatabase = {
-  'robot-city': robotCity
+  'robot-city': robotCity,
+  'universe-113': universeImg
 }
 
 const utilityImageDatabase: ImageDatabase = {
