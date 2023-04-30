05ecc08e853cbae58b71fbc276386fa5d9b75778
mbright90 <michael.bright90@icloud.com>
2023-04-30 15:38:01 +0100

    Add time result to victory screen

--- src/components/victoryScreen/VictoryScreen.tsx
+++ src/components/victoryScreen/VictoryScreen.tsx
@@ -3,14 +3,22 @@ import React, { useContext } from 'react'
 
 import style from './VictoryScreen.module.scss'
 
+const formatTimer = (timeCount: number) => {
+  const minutes = Math.floor(timeCount / 60).toLocaleString(undefined, { minimumIntegerDigits: 2 })
+  const seconds = (timeCount % 60).toLocaleString(undefined, { minimumIntegerDigits: 2 })
+
+  return `${minutes}:${seconds}`
+}
+
 const VictoryScreen: React.FC = () => {
-  const { resetGame } = useContext(AppContext)
+  const { resetGame, timeCount } = useContext(AppContext)
 
   return (
     <div className={style.victoryScreen}>
       <div className={style.victoryContainer}>
         <h1 className={style.victoryHeader}>Congratulations!</h1>
         <p className={style.victoryPara}>You have found all the characters</p>
+        <p className={style.victoryTime}>{formatTimer(timeCount)}</p>
         <button className={style.resetGameButton} onClick={resetGame}>
           Select Image
         </button>
